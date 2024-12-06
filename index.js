95dbff9a49a41bd311f94cbafcf4daad58557cc7
Liz Loomans <162842106+Liziloo@users.noreply.github.com>
2024-12-06 17:28:42 -0500

    Add card footer buttons

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const myLibrary = [];
 const container = document.querySelector('#container');
 
-function Book(title, author, pageCount, read) {
+function Book(title, author, pageCount = '', read, thoughts = '') {
     this.title = title;
     this.author = author;
     this.pageCount = pageCount;
     this.read = read;
+    this.thoughts = thoughts;
     this.info = function() {
         let readString;
         if (read === true) {
@@ -17,24 +18,25 @@ function Book(title, author, pageCount, read) {
     }
 }
 
-function addBookToLibrary(title, author, pageCount, read) {
-    const newBook = new Book(title, author, pageCount, read);
+function addBookToLibrary(title, author, pageCount, read, thoughts) {
+    const newBook = new Book(title, author, pageCount, read, thoughts);
     myLibrary.push(newBook);
 }
 
-addBookToLibrary('Lord of the Rings', 'J.R.R. Tolkien', 1150, true);
-addBookToLibrary('Pride and Prejudice', 'Jane Austen', 448, true);
-addBookToLibrary('Things You Save in a Fire', 'Katherine Center', 320, true);
-addBookToLibrary('The Sparrow', 'Mary Doria Russell', 528, true);
-addBookToLibrary('The Art of the Deal', 'Donald Trump', 372, false);
+addBookToLibrary('Lord of the Rings', 'J.R.R. Tolkien', 1150, true, "The only book I've ever re-read yearly");
+addBookToLibrary('Pride and Prejudice', 'Jane Austen', 448, true, "I like it enough to have read more times htan I can recall. It's a comfort read for me. My favorite adaptation is the BBC miniseries.");
+addBookToLibrary('Things You Save in a Fire', 'Katherine Center', 320, true, "One of the first 'chick-lit'/romance books I read. Made me fall in love with the author's works. Although I can't stand the film adaptation of 'Happiness for Beginners'. It's atrocious.");
+addBookToLibrary('The Sparrow', 'Mary Doria Russell', 528, true, "This is a strange one. Sci-fi, very introspective. Very odd premise. I very much liked it, though I have trouble articulating why. It's very well written.");
+addBookToLibrary('Twilight', 'Stephenie Meyer', 372, false, 'Just no.');
 addBookToLibrary('Arrows of the Queen', 'Mercedes Lackey', 320, true);
 
 function displayBooks() {
 
-    myLibrary.forEach((book) => {
+    myLibrary.map((book, index) => {
 
         const newCard = document.createElement('div');
         newCard.classList.add('book-card');
+        newCard.id = `book-${index}`;
         newCard.setAttribute('data-read', book.read);
 
         const newTitleDiv = document.createElement('div');
@@ -47,14 +49,33 @@ function displayBooks() {
         const newPageCount = document.createElement('div');
         newPageCount.classList.add('book-page-count');
         newPageCount.innerHTML = book.pageCount;
-
         newTitleDiv.append(newTitle, newPageCount);
-        newCard.appendChild(newTitleDiv);
 
         const newAuthor = document.createElement('div');
         newAuthor.classList.add('book-author');
         newAuthor.innerHTML = book.author;
-        newCard.appendChild(newAuthor);
+
+        const newThoughts = document.createElement('div');
+        newThoughts.classList.add('thoughts');
+        newThoughts.innerHTML = book.thoughts;
+
+        const newFooter = document.createElement('div');
+        newFooter.classList.add('card-footer');
+        const markReadButton = document.createElement('button');
+        markReadButton.classList.add('mark-read');
+        if (book.read) {
+            markReadButton.innerHTML = 'Mark as Unread';
+        } else {
+            markReadButton.innerHTML = 'Mark as Read';
+        }
+        
+
+        const deleteButton = document.createElement('button');
+        deleteButton.classList.add('delete-button');
+        deleteButton.innerHTML = 'Delete from Library';
+
+        newFooter.append(markReadButton, deleteButton);
+        newCard.append(newTitleDiv, newAuthor, newThoughts, newFooter);
 
         container.appendChild(newCard);
     })
